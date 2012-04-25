4e47737bdf7893d06cfc595cecf6ebf25d48fca5
Michael Babich <michaelbabich7@gmail.com>
2012-04-25 19:24:07 -0400

    Adding a function that uses difflib to act like Unix's diff on Python strings.

---
+++ avenue/revision.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2012 Michael Babich
+# See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
+
+'''Uses difflib to create a revision system of text files so that only
+the most recent revision is saved in complete form, using a diff for
+the older revisions.
+'''
+from difflib import unified_diff
+
+def diff(string1, string2):
+    '''Returns a compact diff string of the two strings provided.
+    '''
+    def _string_to_list(string):
+        '''Converts the string into a list with a trailing newline,
+        which is the form that unified_diff() understands.
+        '''
+        if string[-1] != '\n':
+            string = '%s%s' % (string, '\n')
+
+        return string.splitlines(True)
+
+    return ''.join(list(unified_diff(_string_to_list(string1),
+                                     _string_to_list(string2))))
