ab2734e2847df771a4a01895531720181866b645
Michael Babich <michaelbabich7@gmail.com>
2012-04-25 00:32:42 -0400

    Copying the basic structure of a Flask application from Federation.

---
+++ avenue/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2012 Michael Babich
+# See LICENSE.txt or http://www.opensource.org/licenses/mit-license.php
+
+'''The avenue package contains everything that is necessary to run an
+Avenue server.
+
+It utilizes a SQL database using SQLAlchemy. It then presents public
+data via Flask and WSGI. It provides web pages under multiple
+different themes and a JSON API for external applications to access.
+'''
+from flask import Flask
+
+app = Flask(__name__)
---
+++ server.wsgi
@@ -0,0 +1,6 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+from avenue import app as application
