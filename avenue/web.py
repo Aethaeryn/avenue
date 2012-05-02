c053b976a230c958b8d8dd968ce9dde6a9c6ee2f
Michael Babich <michaelbabich7@gmail.com>
2012-05-02 14:44:44 -0400

    Using Google's font system to provide a consistent experience across platforms and operating systems. Adding some sample text to test the rendering.

--- avenue/web.py
+++ avenue/web.py
@@ -10,6 +10,25 @@ from avenue import app, api
 def index():
     '''Serves the main page of the website.
     '''
-    words = 'Hello, world!'
+    words = '<h1>Welcome to Avenue</h1><p>Avenue is a new way to run a ' \
+    'website. Don\'t run many independent web applications that are designed '\
+    ' for their own, isolated ecosystems! Avenue makes your life simple by '\
+    'providing minimalist content systems that integrate seamlessly into one '\
+    'Python web application with one database. No content mode is more '\
+    'privileged than another. With Avenue, you\'re not forced to use an '\
+    'application for something that it\'s not designed for. You don\'t get a '\
+    'good user experience if you try to discuss in a wiki application or '\
+    'blog in a forum application.<br><br>Everything fits together in one '\
+    'website with a shared theme set and a unified account system. The '\
+    'entire site just works out of the box without having to hand-modify or '\
+    'install any plugins to get different apps, intended for different '\
+    'purposes, to talk to each other. There\'s a consistent user interface, '\
+    'too. Of course, if you want to extend Avenue, it is designed to handle '\
+    'that. It should be able to support browser-based HTML 5 games, web '\
+    'email, and possibly even IRC clients!<br><br>Avenue is convergence for '\
+    'web apps. Why handle text a dozen different ways if you don\'t have '\
+    'to?</p>'
 
-    return api.make_page(style='static/dark-plain', body=words)
+    return api.make_page(style='static/dark-plain',
+                         body=words,
+                         title='Welcome :: Avenue')
