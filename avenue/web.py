e5b1fc7b9e7fa2dfd660d5d4119c31b07fd984b4
Michael Babich <michaelbabich7@gmail.com>
2012-05-31 20:27:27 -0400

    Using the simpler, shorter description from the website.

--- avenue/web.py
+++ avenue/web.py
@@ -33,24 +33,7 @@ background1 = {'plain'   : '#000000',
                'article' : '#d6d9d9',
                'box1'    : '#1a1123'}
 
-words = '<h1>Welcome to Avenue</h1><p><a href="/">Avenue</a> is a new way to run a ' \
-    'website. Don\'t run many independent web applications that are designed '\
-    ' for their own, isolated ecosystems! Avenue makes your life simple by '\
-    'providing minimalist content systems that integrate seamlessly into one '\
-    'Python web application with one database. No content mode is more '\
-    'privileged than another. With Avenue, you\'re not forced to use an '\
-    'application for something that it\'s not designed for. You don\'t get a '\
-    'good user experience if you try to discuss in a wiki application or '\
-    'blog in a forum application.</p><p>Everything fits together in one '\
-    'website with a shared theme set and a unified account system. The '\
-    'entire site just works out of the box without having to hand-modify or '\
-    'install any plugins to get different apps, intended for different '\
-    'purposes, to talk to each other. There\'s a consistent user interface, '\
-    'too. Of course, if you want to extend Avenue, it is designed to handle '\
-    'that. It should be able to support browser-based HTML 5 games, web '\
-    'email, and possibly even IRC clients!</p><p>Avenue is convergence for '\
-    'web apps. Why handle text a dozen different ways if you don\'t have '\
-    'to?</p>'
+words = '<h1>Zombie Raptor Launches on August 20th</h1><p>Expect awesome games from Zombie Raptor in the near future.</p>'
 
 @app.route('/')
 def index():
