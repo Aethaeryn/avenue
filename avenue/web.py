31e0aa56ed43617bb7d10cd1cd1a49a067899b5d
Michael Babich <michaelbabich7@gmail.com>
2012-09-28 02:12:45 -0400

    Move the sample thread to an external data file; will eventually replace this with a real database.

--- avenue/web.py
+++ avenue/web.py
@@ -57,42 +57,13 @@ def index():
 def forums():
     page_title = '%s :: %s' % ('Forums', heading)
 
-    thread_title = 'This is a Sample Thread'
-
-    post_list = ['<p>The forums will go here eventually. I\'m just testing things out now. As you can see, the first post in a thread gets special treatment.</p>',
-                 '<p>Well, I could\'ve just put a note explaining that this website is just full of static filler stuff as I finalize the design aspect of Avenue, but then I realized that this site\'s purpose is to render text. So, I can simply write that explanation here in a verbose way. That way, I can test out posts of all different sizes to see how the theme renders.</p>', '<p>Fortunately, this is easier than it looks.</p>',
-                 '<p>So a typical forum thread would look like this, except hopefully people would put lengthier posts here. The site and the software this site is built on top of are still being written, so this is just placeholder text. That means that this site doesn\'t actually work yet. Please don\'t be angry if one of the links doesn\'t work because nothing really works here yet. It\'s just sample text.</p>',
-                 '<p>The thing is, I need to have a lot of text and a lot of posts in order to fully test out how everything is going to work or else I will not be able to account for all situations. I could put nonsense here or just some section from a public domain work, but to be honest, writing nonsense text is actually pretty easy.</p><p>Besides, this way I can try to think of all of the typical use cases that a forum might see in terms of posts and try to write as many different types of posts here. This will provide variety in testing.</p>', '<p>So, hopefully any style bugs will show up now before this is used by other people in a production environment.</p>',
-                 '<p>Almost there! I just need to fill up an entire page!</p>',
-                 '<p>Now, I am going to cheat a bit and have a bunch of one-liners.</p>',
-                 '<p>Alright!</p>',
-                 '<p>So this is what a page looks like when you have more posts than can fit on a typical screen. This is the threaded view, too!</p>',
-                 '<p>Now</p>',
-                 '<p>let\'s</p>',
-                 '<p>go</p>',
-                 '<p>max</p>',
-                 '<p>depth.</p>',
-                 '<p>The tests are now over.</p>']
-
-    posts = []
-
-    for post in post_list:
-        posts.append({'content' : post, 'level' : 1})
-
-    posts[0]['level'] = 0
-    posts[2]['level'] = 2
-    posts[4]['level'] = 2
-    posts[5]['level'] = 3
-    posts[7]['level'] = 2
-    posts[8]['level'] = 3
-    posts[11]['level'] = 2
-    posts[12]['level'] = 3
-    posts[13]['level'] = 4
-    posts[14]['level'] = 5
+    sample = open(path.join(path.dirname(__file__), 'data', 'sample.yml'))
+    thread = yaml.load(sample)
+    sample.close()
 
     post_author = 'Michael :: -1 year ago'
 
-    return render_template('forum.html', style='night', main_title=heading, posts=posts, sidebar=navbar, title=page_title, author=post_author, thread_title=thread_title)
+    return render_template('forum.html', style='night', main_title=heading, posts=thread['posts'], sidebar=navbar, title=page_title, author=post_author, thread_title=thread['title'])
 
 @app.route('/night.css')
 def night():
