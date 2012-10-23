2ce7a6f4057d76d6ba2e146846218d4194ec78d4
Michael Babich <michaelbabich7@gmail.com>
2012-10-23 15:57:51 -0400

    Begin to rewrite database tables to match new data design.

--- avenue/database/tables.py
+++ avenue/database/tables.py
@@ -4,24 +4,43 @@
 '''Defines the tables used by the database.
 '''
 
-from sqlalchemy import Table, Integer, ForeignKey, DateTime, Column, String
+from sqlalchemy import Table, Integer, ForeignKey, DateTime, Column, String, Boolean
 
 def get_tables(metadata):
     table = {}
 
-    # Store parent/children/revisions/tags/content-type
+    # TODO: posts, threads, users, urls, navbar, tags, styles
+    # TODO: Work a per-forum karma system.
+
+    # TODO: Work on a post revision system and a way to handle first
+    # tags specially as content type.
+
+    # TODO: Store parent/children/tags
     table['posts'] = Table('posts', metadata,
                            Column('id', Integer, primary_key=True),
                            Column('author', ForeignKey('users.username')),
                            Column('karma', Integer),
                            Column('content', String),
+                           Column('date', DateTime),
                            Column('parent', ForeignKey('posts.id')))
+
+    # TODO: Store tags/threaded/author... read it from 'start' post.
+    table['thread'] = Table('thread', metadata,
+                            Column('id', Integer, primary_key=True),
+                            Column('start', ForeignKey('posts.id')),
+                            Column('title', String))
 
     table['users'] = Table('users', metadata,
                            Column('id', Integer, primary_key=True),
                            Column('username', String, unique=True),
                            Column('joined', DateTime),
-                           Column('email', String),
-                           Column('karma', Integer))
+                           Column('email', String))
+
+    table['nav'] = Table('nav', metadata,
+                         Column('id', Integer, primary_key=True),
+                         Column('visible', Boolean),
+                         Column('title', String),
+                         Column('text', String),
+                         Column('link', String))
 
     return table
