b7951edd07e3f276a78b11258d2fc440ff4a6a2e
Michael Babich <michaelbabich7@gmail.com>
2012-10-25 16:54:34 -0400

    Use 'create_table' as the function name because it matches the SQL function.

--- avenue/database/tables.py
+++ avenue/database/tables.py
@@ -9,7 +9,7 @@ from sqlalchemy import Table, Integer, ForeignKey, DateTime, Column, String, Boo
 def get_tables(metadata):
     table = {}
 
-    def add_table(name, *columns):
+    def create_table(name, *columns):
         table[name] = Table(name, metadata,
                             Column('id', Integer, primary_key=True),
                             *columns)
@@ -21,35 +21,35 @@ def get_tables(metadata):
     # tags specially as content type.
 
     # TODO: Store parent/children/tags
-    add_table('posts',
-              Column('author', ForeignKey('users.username')),
-              Column('karma', Integer),
-              Column('content', String),
-              Column('date', DateTime),
-              Column('parent', ForeignKey('posts.id')))
+    create_table('posts',
+                 Column('author', ForeignKey('users.username')),
+                 Column('karma', Integer),
+                 Column('content', String),
+                 Column('date', DateTime),
+                 Column('parent', ForeignKey('posts.id')))
 
     # TODO: Store tags/threaded/author... read it from 'start' post.
-    add_table('thread',
-              Column('start', ForeignKey('posts.id')),
-              Column('title', String))
+    create_table('thread',
+                 Column('start', ForeignKey('posts.id')),
+                 Column('title', String))
 
-    add_table('users',
-              Column('username', String, unique=True),
-              Column('joined', DateTime),
-              Column('email', String))
+    create_table('users',
+                 Column('username', String, unique=True),
+                 Column('joined', DateTime),
+                 Column('email', String))
 
-    add_table('nav',
-              Column('visible', Boolean),
-              Column('title', String),
-              Column('text', String),
-              Column('link', String))
+    create_table('nav',
+                 Column('visible', Boolean),
+                 Column('title', String),
+                 Column('text', String),
+                 Column('link', String))
 
-    add_table('tags',
-              Column('text', String),
-              Column('color', String))
+    create_table('tags',
+                 Column('text', String),
+                 Column('color', String))
 
-    add_table('forums',
-              Column('name', String),
-              Column('url', String))
+    create_table('forums',
+                 Column('name', String),
+                 Column('url', String))
 
     return table
