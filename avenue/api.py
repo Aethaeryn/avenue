6eed96d757631eece025e58afb3e098ac5495b2d
Michael Babich <michaelbabich7@gmail.com>
2013-04-29 21:57:47 -0400

    Remove currently unused functions.

--- avenue/api.py
+++ avenue/api.py
@@ -2,9 +2,8 @@
 things that Avenue imports (e.g. Federation) so that they use the same
 Flask instance as Avenue.
 '''
-from flask import json, request, make_response, render_template
-from docutils.core import publish_parts
-from os import path, listdir
+from flask import json, make_response, render_template
+from os import path
 import yaml
 
 def read_data(filename):
@@ -34,50 +33,3 @@ def make_css(theme):
     response = make_response(render_template('main.css', theme=theme))
     response.mimetype = 'text/css'
     return response
-
-def check_cookie():
-    '''Checks the cookie for the appropriate user.
-
-    If there's no cookie, then the user is None.
-    '''
-    cookie = request.cookies.get('username')
-
-    if cookie == 'michael':
-        return True
-
-    else:
-        return False
-
-def make_page(**kwargs):
-    '''Used by plugins to Avenue, such as games, to generate a page
-    integrated into the rest of the website.
-    '''
-    return render_template('basic.html', **kwargs)
-
-def get_header():
-    '''Gets the custom HTML from templates/header.html, if it exists.
-    '''
-    template_path = path.join(path.dirname(__file__), 'templates')
-    templates     = listdir(template_path)
-
-    if 'header.html' in templates:
-        header = open(path.join(template_path, 'header.html'), 'r')
-        text   = header.read()
-
-        header.close()
-        return text
-
-    else:
-        return ''
-
-def markup_to_html(string):
-    '''Use docutils to turn the reStructuredText markup language into
-    HTML that can be used elsewhere. Less than and greater than signs
-    are properly escaped so the returned unicode string can be trusted
-    in the HTML templates. Unicode is always returned, even if an
-    ordinary string is passed in.
-    '''
-    #### TODO: The way headings are handled is currently not ideal and
-    #### sometimes headings will need 'html_body' instead of 'body' to
-    #### even show up here at all!
-    return publish_parts(string, writer_name='html')['body']
